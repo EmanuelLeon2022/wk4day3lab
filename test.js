44ad74d75ba21f6337f09b704e1275f579cc30f7
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-10-26 13:01:32 -0400

    Update test.js
    "Fizz Buzz Answered"

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 for (let i=0; i <= 100 ; i = i + 1){
-    if (i % 3 == 0){
-        console.log("Fizz");
-    }
-    else if (i % 5 == 0){
-        console.log("Buzz");
-    }
-    if((i % 5==0) && (i % 3==0))console.log("FizzBuzz")
-    else(console.log(i));
+    if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    if((i % 5==0) && (i % 3==0))console.log("FizzBuzz");
+    else if(i % 3 != 0 && i %5 != 0) console.log(i);
 }
